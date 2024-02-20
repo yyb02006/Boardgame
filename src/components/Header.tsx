02dae09a8709bd6bf256e0d340e14bf7ee842c37
alwjrqns <yyb02006@gmail.com>
2024-02-21 05:17:37 +0900

    게임 룰 컴포넌트 애니메이션 추가 및 내용 작성

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { useAppContext } from '#AppContext';
+import useToggle from '#hooks/useToggle';
 import { fadeInZ } from '#styles/animations';
 import { fullWidthHeight } from '#styles/theme';
 import React, { useEffect, useState } from 'react';
@@ -74,7 +75,7 @@ const Spread = styled.div`
 	font-size: 1rem;
 	&.Open {
 		${fadeInZ({
-			name: 'open',
+			name: 'openMenu',
 			distance: 200,
 			duration: 300,
 			seqDirection: 'normal',
@@ -82,7 +83,7 @@ const Spread = styled.div`
 	}
 	&.Closed {
 		${fadeInZ({
-			name: 'close',
+			name: 'closeMenu',
 			distance: 200,
 			duration: 300,
 			seqDirection: 'reverse',
@@ -144,9 +145,64 @@ interface HeaderProps {
 const Rules = styled.div`
 	font-weight: 300;
 	cursor: pointer;
+	&:hover {
+		color: #e9a71a;
+	}
+	&.Open {
+		> span {
+			color: #e9a71a;
+		}
+	}
 `;
 
-const LogoSection = styled.section`
+const RuleModalLayout = styled.div`
+	position: absolute;
+	width: 400px;
+	top: 80px;
+	left: 48px;
+	background-color: #202020;
+	box-shadow: 0 0 24px #101010;
+	border-radius: 16px;
+	color: #eaeaea;
+	padding: 24px 32px;
+	> ul {
+		list-style: none;
+		margin: 0;
+		padding: 0;
+		font-size: 0.875rem;
+		> li {
+			margin-bottom: 12px;
+			line-height: 1.5rem;
+		}
+	}
+	> .Title {
+		font-weight: 800;
+		font-size: 2rem;
+		margin-bottom: 16px;
+		color: #e9a71a;
+	}
+	&.Open {
+		${fadeInZ({
+			name: 'openMenu',
+			distance: 200,
+			duration: 300,
+			seqDirection: 'normal',
+		})}
+	}
+	&.Closed {
+		${fadeInZ({
+			name: 'closeMenu',
+			distance: 200,
+			duration: 300,
+			seqDirection: 'reverse',
+		})}
+	}
+	@media screen and (max-width: 1024px) {
+		left: 0;
+	}
+`;
+
+const LogoSectionLayout = styled.section`
 	display: flex;
 	gap: 48px;
 `;
@@ -165,33 +221,96 @@ const LinkWrapper = ({ to, label, page }: { to: string; label: string; page: Pag
 	);
 };
 
+const RuleModal = ({ isModalVisible }: { isModalVisible: boolean }) => {
+	const { pageState } = useAppContext();
+	const rules = {
+		borderGame: {
+			name: 'BorderGame',
+			descriptions: [
+				'상대보다 많은 땅을 소유하면 이기는 게임입니닷',
+				'첫 경계를 놓은 이후에는 내가 소유한 경계에 이웃한 경계만 선택할 수 있습니닷',
+				'상대방이 연결한 경계를 뚫고 경계를 소유할 수 없습니닷',
+				'더 이상 승부가 변경될 가능성이 없다면 게임은 자동으로 종료됩니닷',
+			],
+		},
+		cardFlipper: {
+			name: 'CardFlipper',
+			descriptions: [
+				'모든 카드를 뒤집고 더 좋은 기록을 내는 게임입니닷',
+				'두 개의 카드를 뒤집었을 때 서로 같다면 점수로 인정되고, 아니라면 카드는 다시 뒤집힙니닷',
+				'카드가 모두 뒤집혔다면 게임은 종료되고, 그때까지의 시간과 시도횟수를 확인할 수 있습니닷',
+			],
+		},
+		othello: {
+			name: 'Othello',
+			descriptions: [
+				'보드위의 돌을 더 많이 소유하면 이기는 게임입니닷',
+				'돌을 놓았을 때 직선 네 방향 중 상대방의 돌을 중간에 끼고 맞은 편에 내 돌이 있다면 사이의 돌은 모두 내 소유가 됩니닷',
+				'상대방의 돌을 뒤집을 수 있는 곳에만 돌을 놓을 수 있습니닷',
+				'이미 놓여진 상대방의 돌을 뒤집을 수 있는 기회가 5번 주어지며 이 기회는 자기 소유의 8개의 돌이 있어야 사용할 수 있습니닷',
+				'직전에 놓였거나 뒤집혔던 돌은 위 규칙으로 뒤집을 수 없습니닷',
+				'더 이상 돌을 놓거나 상대방의 돌을 뒤집을 수 없다면 턴이 넘어가고, 양 플레이어 모두 그렇다면 게임이 종료됩니닷',
+			],
+		},
+	};
+	return (
+		<RuleModalLayout className={isModalVisible ? 'Open' : 'Closed'}>
+			<div className="Title">{pageState !== 'home' ? `${rules[pageState].name} Rule` : ''}</div>
+			<ul>
+				{pageState !== 'home'
+					? rules[pageState].descriptions.map((desc, id) => <li key={id}>{`${id}. ${desc}`}</li>)
+					: null}
+			</ul>
+		</RuleModalLayout>
+	);
+};
+
+const LogoSection = ({ title }: HeaderProps) => {
+	const { pageState } = useAppContext();
+	const {
+		toggle: isModalOpen,
+		isVisible: isModalVisible,
+		toggleHandler: modalToggleHandle,
+		setToggle: setIsModalOpen,
+		setIsVisible: setIsModalVisible,
+	} = useToggle();
+	useEffect(() => {
+		setIsModalOpen(false);
+		setIsModalVisible(false);
+	}, [pageState]);
+	return (
+		<LogoSectionLayout>
+			<HeaderNav to={'/'}>{title}</HeaderNav>
+			<Rules
+				onClick={() => {
+					modalToggleHandle(!isModalOpen);
+				}}
+				className={isModalVisible ? 'Open' : 'Closed'}
+			>
+				<span>Rules</span>
+				{isModalOpen ? <RuleModal isModalVisible={isModalVisible} /> : null}
+			</Rules>
+		</LogoSectionLayout>
+	);
+};
+
 const Header = ({ title }: HeaderProps) => {
 	const { pageState } = useAppContext();
-	const [isOpen, setIsOpen] = useState(false);
-	const [isVisible, setIsVisible] = useState(false);
-	const menuToggleHandler = (isOpen: boolean) => {
-		if (isOpen) {
-			setIsOpen(true);
-			setIsVisible(true);
-		} else {
-			setIsVisible(false);
-			setTimeout(() => {
-				setIsOpen(false);
-			}, 300);
-		}
-	};
+	const {
+		toggle: isMenuOpen,
+		isVisible: isMenuVisible,
+		toggleHandler: menuToggleHandler,
+		setToggle: setIsMenuOpen,
+	} = useToggle();
 	useEffect(() => {
-		setIsOpen(false);
+		setIsMenuOpen(false);
 	}, [pageState]);
 	return (
 		<Layout>
-			<LogoSection>
-				<HeaderNav to={'/'}>{title}</HeaderNav>
-				<Rules>Rules</Rules>
-			</LogoSection>
+			<LogoSection title={title}></LogoSection>
 			<HambergerMenu>
-				{isOpen ? (
-					<Spread className={isVisible ? 'Open' : 'Closed'}>
+				{isMenuOpen ? (
+					<Spread className={isMenuVisible ? 'Open' : 'Closed'}>
 						<CloseButton
 							onClick={() => {
 								menuToggleHandler(false);
